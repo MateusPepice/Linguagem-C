4619f24360f8d858309691dfd73bc9e8e572c7ad
MateusPepice <112258589+MateusPepice@users.noreply.github.com>
2023-01-05 22:06:34 -0300

    Add files via upload

---
+++ Imprimindo dados (arq. TXT)/Exercício 03.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+struct exercicio3{
+    char aluno[30];
+    float nota1, nota2, media;
+}alunos[10];
+
+    int i;
+    float soma;
+    FILE *ex3;
+    ex3 = fopen("Notas.txt", "w");
+
+if (ex3 == NULL) {
+    printf("Erro ao abrir o arquivo!");
+    system("pause");
+    return 0;
+}
+
+for (i=1; i<=2; i++) {
+    printf("Informe o nome do aluno %i: ", i);
+        fflush(stdin);
+        gets(alunos[i].aluno);
+        fflush(stdin);
+        fputs(alunos[i].aluno, ex3);
+    printf("Informe a nota 1: ");
+        scanf("%f", &alunos[i].nota1);
+        fprintf(ex3,"%2.f", alunos[i].nota1);
+    printf("Informe a nota 2: ");
+        scanf("%f", &alunos[i].nota2);
+        fprintf(ex3,"%2.f", alunos[i].nota2);
+
+        alunos[i].media = (alunos[i].nota1 + alunos[i].nota2) / 2;
+        fprintf(ex3, " %.2f", alunos[i].media);
+        fprintf(ex3, "\n");
+
+}
+    fclose(ex3);
+}
